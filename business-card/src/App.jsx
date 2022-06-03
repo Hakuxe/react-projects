2bc1264a18bb5c27f679d0cb3f4ff3f0a7253123
Hakuxe <arthurnorton.jobs@gmail.com>
2022-06-03 11:50:17 -0300

    criando projeto

---
+++ business-card/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './styles/app.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello word</h1>
+    </div>
+  )
+}
+
+export default App
