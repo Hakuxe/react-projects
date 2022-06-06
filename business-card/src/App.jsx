f9c41f8cd788cbe2b3f46ec98daedba063c1b42b
Hakuxe <arthurnorton.jobs@gmail.com>
2022-06-06 15:13:06 -0300

    finalizando projeto base

--- business-card/src/App.jsx
+++ business-card/src/App.jsx
@@ -4,13 +4,26 @@ import "./styles/app.css";
 
 function App() {
 	return (
-		<div className="App">
-			<BusinesCard 
-				picture={'https://picsum.photos/550/300'}
-				name={"Laura Smith"}
-				job={"Frontend Developer"}
-				extraInfo={"laurasmith.website"}
-			/>
+		<div className="grid">
+			<div className="containerApp">
+				<BusinesCard
+					picture={"https://picsum.photos/550/300"}
+					name={"Laura Smith"}
+					job={"Frontend Developer"}
+					extraInfo={"laurasmith.website"}
+				/>
+			</div>
+
+			<div className="containerApp">
+				<BusinesCard
+					picture={"https://picsum.photos/550/300"}
+					name={"Laura Smith"}
+					job={"Frontend Developer"}
+					extraInfo={"laurasmith.website"}
+				/>
+			</div>
+
+			
 		</div>
 	);
 }
