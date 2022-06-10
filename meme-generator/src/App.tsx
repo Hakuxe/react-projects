17ecd9df39c2114e61ac7e32a69b039a54f1dc7b
Hakuxe <arthurnorton.jobs@gmail.com>
2022-06-10 13:31:13 -0300

    criando projeto gerador de memes

---
+++ meme-generator/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+import './styles/app.css'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>header</h1>
+    </div>
+  )
+}
+
+export default App
