269fcca45a9424d07045ace69ee5699993db50f5
Hakuxe <arthurnorton.jobs@gmail.com>
2022-07-01 18:02:31 -0300

    testando formas de lidar com erros

--- forms/rhook-form/src/components/SignIn.tsx
+++ forms/rhook-form/src/components/SignIn.tsx
@@ -1,8 +1,10 @@
 import { SubmitHandler, useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
 type LoginInputs = {
 	username: string;
 	password: string;
+	age: number;
 };
 
 export function SignIn() {
@@ -12,12 +14,85 @@ export function SignIn() {
 		formState: { errors },
 	} = useForm<LoginInputs>();
 
-	const onSubmit: SubmitHandler<LoginInputs> = (data) => console.log(data);
+	const onSubmit: SubmitHandler<LoginInputs> = (data) =>
+		console.log(data, errors);
 
 	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<input type="text" className="border border-gray-500" placeholder="Usuário" {...register("username")}/>
-			<input type="text" className="border border-gray-500" placeholder="Senha" {...register("password")}/>
+		<form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+			<label htmlFor="user"> Usuário: </label>
+			<input
+				id="user"
+				type="text"
+				className="border border-gray-300 p-3"
+				placeholder="Digite seu usuário"
+				{...register("username", { required: true })}
+			/>
+			{errors.username && "Campo obrigatório"}
+			{errors.username && (
+				<span className="text-sm text-red-500 ">Campo obrigatório</span>
+			)}
+			<p>{errors.username?.message} </p>
+
+			<label htmlFor="password" className="mt-5">
+				Senha:
+			</label>
+			<input
+				id="password"
+				type="text"
+				className="border border-gray-300 p-3"
+				placeholder="Digite sua senha"
+				{...register("password", { required: "Este campo é obrigatório" })}
+			/>
+			<ErrorMessage
+				errors={errors}
+				name="password"
+				message={errors.username?.message} // não da pra estilizar
+			/>
+			<ErrorMessage
+				errors={errors}
+				name="password"
+				render={({ message }) => (
+					// message vindo do object required
+					<p className="text-sm text-red-500">Campo obrigatório</p>
+				)}
+			/>
+			<label htmlFor="user" className="mt-5">
+				Idade:
+			</label>
+			<input
+				id="age"
+				type="number"
+				className="border border-gray-300 p-3"
+				placeholder="Digite sua idade"
+				{...register("age", {
+					required: "This input is required.",
+					pattern: {
+						value: /\d+/,
+						message: "This input is number only.",
+					},
+					maxLength: {
+						value: 10,
+						message: "This input exceed maxLength.",
+					},
+				})}
+			/>
+			<ErrorMessage
+				errors={errors}
+				name="age"
+				render={({ messages }) =>
+					messages &&
+					Object.entries(messages).map(([type, message]) => (
+						<p key={type}>{message}</p>
+					))
+				}
+			/>
+
+			<button
+				type="submit"
+				className="w-full rounded bg-purple-600 hover:bg-purple-700 transition-colors py-4 mt-8 text-white font-bold"
+			>
+				Entrar
+			</button>
 		</form>
 	);
 }
