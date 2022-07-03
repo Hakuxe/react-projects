7583360279ec61d50a06cd499e87622c696f15b2
Hakuxe <arthurnorton.jobs@gmail.com>
2022-07-03 11:32:25 -0300

    testando validações com yup

--- forms/rhook-form/src/components/YupForm.tsx
+++ forms/rhook-form/src/components/YupForm.tsx
@@ -1,12 +1,61 @@
-import React from 'react';
+import React from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+
+type YupInputTypes = {
+	name: string;
+	email: string;
+	age: never;
+	createdOn: Date;
+};
 
 export function YupForm() {
-  return (
-    <form>
-      <input type="text" className='border border-gray-300 p-3'/>
-      <input type="text" className='border border-gray-300 p-3'/>
-      <input type="text" className='border border-gray-300 p-3'/>
-      <input type="text" className='border border-gray-300 p-3'/>
-    </form>
-  )
+	const formValidations = yup.object().shape({
+		name: yup.string().required(),
+		email: yup.string().email(),
+		age: yup.number().required().positive().integer(),
+		createdOn: yup.date().default(function () {
+			return new Date();
+		}),
+	});
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<YupInputTypes>({
+		resolver: yupResolver(formValidations),
+	});
+
+	function onSubmit(data: YupInputTypes) {
+		console.log(data);
+	}
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<input
+				type="text"
+				placeholder="Digite seu nome"
+				className="border border-gray-300 p-3"
+			/>
+			<input
+				type="text"
+				placeholder="Digite seu e-mail"
+				className="border border-gray-300 p-3"
+			/>
+			<input
+				type="number"
+				placeholder="Digite sua idade"
+				className="border border-gray-300 p-3"
+				min={0}
+				max={102}
+			/>
+			<input
+				type="date"
+				placeholder="Digite sua data de nascimento"
+				className="border border-gray-300 p-3"
+			/>
+		</form>
+	);
 }
