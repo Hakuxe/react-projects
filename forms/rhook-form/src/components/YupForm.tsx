09319136ee3941a9dcf2a8249367db57d544ec17
Hakuxe <arthurnorton.jobs@gmail.com>
2022-07-06 12:02:01 -0300

    testando erros

--- forms/rhook-form/src/components/YupForm.tsx
+++ forms/rhook-form/src/components/YupForm.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
+import { setLocale } from 'yup';
 
 type YupInputTypes = {
 	name: string;
@@ -10,11 +11,12 @@ type YupInputTypes = {
 	createdOn: Date;
 };
 
+
 export function YupForm() {
 	const formValidations = yup.object().shape({
 		name: yup.string().required(),
 		email: yup.string().email(),
-		age: yup.number().required().positive().integer(),
+		age: yup.number().integer("Insira um valor inteiro"),
 		createdOn: yup.date().default(function () {
 			return new Date();
 		}),
@@ -27,6 +29,7 @@ export function YupForm() {
 	} = useForm<YupInputTypes>({
 		resolver: yupResolver(formValidations),
 	});
+
 
 	function onSubmit(data: YupInputTypes) {
 		console.log(data);
@@ -50,16 +53,19 @@ export function YupForm() {
 				type="number"
 				placeholder="Digite sua idade"
 				className="border border-gray-300 p-3"
-				min={0}
-				max={102}
+
 				{...register("age")}
 			/>
+			{/* {console.log(errors)} */}
+			{errors.age?.message && <p>{errors.age?.message}</p>}
 			<input
 				type="date"
 				placeholder="Digite sua data de nascimento"
 				className="border border-gray-300 p-3"
 				{...register("createdOn")}
 			/>
+
+			<button type="submit">Enviar</button>
 		</form>
 	);
 }
