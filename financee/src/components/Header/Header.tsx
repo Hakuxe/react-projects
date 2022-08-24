6daae64d5d3a9a688f41ea4dc1994524b41d97ff
Hakuxe <arthurnorton.jobs@gmail.com>
2022-08-24 18:10:19 -0300

    criando componente header

--- financee/src/components/Header/Header.tsx
+++ financee/src/components/Header/Header.tsx
@@ -1,10 +1,9 @@
-import styles from "./header.module.css"
+import styles from "./header.module.css";
 
-export const Header: React.FC = () =>{
-   return(
-      <header>
-         <h1>Finance</h1>
-         <p>deu</p>
-      </header>
-   );
-}
+export const Header: React.FC = () => {
+	return (
+		<header className={styles.headerContainer}>
+			<h1>Financee</h1>
+		</header>
+	);
+};
